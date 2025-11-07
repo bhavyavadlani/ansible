a75af912f6f6f4ecddd3ed08f452f0385bde1521
vadlanibhavya <vadlani.bhavya@gmail.com>
2025-11-07 09:16:43 +0530

    added

--- FRONTEND/src/VoterApp.jsx
+++ FRONTEND/src/VoterApp.jsx
@@ -19,7 +19,7 @@ import {
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function VoterApp() {
-  const baseUrl = `http://localhost:30010`;
+  const baseUrl = import.meta.env.VITE_API_URL;
   const [voter, setVoter] = useState({
     id: "",
     name: "",
